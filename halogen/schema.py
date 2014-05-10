a15491edc06246ab3f88e463b3ae63a3921504ea
grimborg <dev@oscarvilaplana.cat>
2014-05-10 16:36:09 +0200

    remove apply

--- halogen/schema.py
+++ halogen/schema.py
@@ -150,14 +150,7 @@ class _Schema(types.Type):
         return result
 
     @classmethod
-    def deserialize(cls, value):
-        """Deserialize input.
-
-        :param value: Dict of already loaded json which will be deserialized by schema attributes.
-
-        :returns: Dict of deserialized value for attributes. Where key is name of schema's attribute and value is
-        deserialized value from value dict.
-        """
+    def deserialize(cls, value, output=None):
         errors = []
         result = {}
         for attr in cls.__attrs__:
@@ -169,12 +162,11 @@ class _Schema(types.Type):
 
         if errors:
             raise exceptions.ValidationError(errors)
-        return result
 
-    @classmethod
-    def apply(cls, value, result):
+        if output is None:
+            return result
         for attr in cls.__attrs__:
-            attr.accessor.set(result, value[attr.name])
+            attr.accessor.set(output, result[attr.name])
 
 
 class _SchemaType(type):
