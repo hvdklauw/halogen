2c3ca1e5f924f2e706d44f4ea93c92d97851060a
Oleg Pidsadnyi <oleg.podsadny@gmail.com>
2014-05-11 11:57:52 +0200

    tests cleanup

--- tests/deserialize/test_complex_type.py
+++ tests/deserialize/test_complex_type.py
@@ -1,22 +1,55 @@
 import copy
-from decimal import Decimal
+from decimal import Decimal, InvalidOperation
 
 import pytest
 
 import halogen
 import halogen.types
-import halogen.exceptions
+from halogen import exceptions
 
 
-def test_nested(nested_data, nested_schema):
+class Amount(halogen.types.Type):
+    @classmethod
+    def deserialize(cls, value):
+        try:
+            return dict(
+                currency=value["currency"],
+                amount=Decimal(value["amount"])
+            )
+        except InvalidOperation as e:
+            raise exceptions.ValidationError(e)
+
+
+class Person(halogen.Schema):
+    name = halogen.Attr()
+    surname = halogen.Attr()
+
+
+class NestedSchema(halogen.Schema):
+    person = halogen.Attr(Person)
+    is_friend = halogen.Attr()
+    price = halogen.Attr(Amount)
+
+
+def test_nested():
     """Test deserialization of a nested type."""
+
+    nested_data = {
+        "person": {
+            "name": "Johann",
+            "surname": "Gambolputty"
+        },
+        "is_friend": True,
+        "price": {"currency": "EUR", "amount": "13.37"}
+    }
+
     expected = copy.deepcopy(nested_data)
     expected["price"]["amount"] = Decimal(expected["price"]["amount"])
-    deserialized = nested_schema.deserialize(nested_data)
+    deserialized = NestedSchema.deserialize(nested_data)
     assert deserialized == expected
 
 
-def test_invalid_value(nested_schema):
+def test_invalid_value():
     """Test errors reported for a broken nested type."""
     data = {
         "person": {
@@ -36,15 +69,15 @@ def test_invalid_value(nested_schema):
     }
 
     with pytest.raises(halogen.exceptions.ValidationError) as e:
-        nested_schema.deserialize(data)
+        NestedSchema.deserialize(data)
     assert e.value.to_dict() == errors
 
 
-def test_missing_attribute(nested_schema):
+def test_missing_attribute():
     data = {
         "is_friend": True,
         "price": {"currency": "EUR", "amount": "wrong_amount"}
     }
     # TODO improve
     with pytest.raises(halogen.exceptions.ValidationError):
-        nested_schema.deserialize(data)
+        NestedSchema.deserialize(data)
--- tests/serialize/test_serialize.py
+++ tests/serialize/test_serialize.py
@@ -1,31 +1,19 @@
 """Tests for testing serialization of Halogen schemas."""
 
-import pytest
 import halogen
 
 
-@pytest.fixture
-def test_schema():
-    """Test schema for serialization."""
+def test_link_simple():
+    """Check that link is serialized in the simple case."""
 
-    class TestSchema(halogen.Schema):
-        self = halogen.Link(attr="uid")
-
-    return TestSchema
-
-
-@pytest.fixture
-def test_content():
-    """Test content for serialization."""
-    return {
-        "uid": "/test/123"
+    data = {
+        "uid": "/test/123",
     }
 
+    class Schema(halogen.Schema):
+        self = halogen.Link(attr="uid")
 
-def test_content_serialization(test_schema, test_content):
-    """Check that test_content serialized into HAL+json format."""
-
-    assert test_schema.serialize(test_content) == {
+    assert Schema.serialize(data) == {
         "_links": {
             "self": {"href": "/test/123"}
         }
