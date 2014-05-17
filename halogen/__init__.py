2a69f1d4f225c64bb70dcf3ffd29cf83aa9040cd
Oleg Pidsadnyi <oleg.podsadny@gmail.com>
2014-05-18 01:03:14 +0200

    Export accessor

--- halogen/__init__.py
+++ halogen/__init__.py
@@ -1,7 +1,8 @@
-from .schema import Schema, Attr, Link, Embedded
+from .schema import Schema, Attr, Link, Embedded, Accessor
 from . import types
 
 __all__ = [
+    "Accessor",
     "Schema",
     "Attr",
     "Link",
