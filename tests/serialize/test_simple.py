6c9f0faf604bec6998e1f47e2acdf78860b29bea
Andrey Makhnach <andrey.makhnach@paylogic.eu>
2014-05-21 13:26:52 +0200

    Added support of curies.

--- tests/serialize/test_simple.py
+++ tests/serialize/test_simple.py
@@ -18,3 +18,38 @@ def test_link_simple():
             "self": {"href": "/test/123"}
         }
     }
+
+
+def test_link_namespace():
+    """Check that link is serialized with namespace."""
+
+    data = {
+        "uid": "/test/123",
+    }
+
+    class Schema(halogen.Schema):
+        self = halogen.Link(attr="uid", namespace="test")
+
+    assert Schema.serialize(data) == {
+        "_links": {
+            "test:self": {"href": "/test/123"}
+        }
+    }
+
+
+def test_curies():
+
+    class Schema(halogen.Schema):
+
+        curies = halogen.Curies([
+            halogen.Curie(name="acme", href="/test/123")
+        ])
+
+    assert Schema.serialize({}) == {
+        "_links": {
+            "curies": [{
+                "name": "acme",
+                "href": "/test/123",
+            }]
+        }
+    }
